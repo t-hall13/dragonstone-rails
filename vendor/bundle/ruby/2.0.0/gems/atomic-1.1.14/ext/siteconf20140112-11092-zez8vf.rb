b9b2326d3edaedd90a055f91b29d1ee192f6bb9a
Tim Hall <timothypatrickhall@gmail.com>
2014-01-12 13:52:51 -0800

    Atomic added

--- vendor/bundle/ruby/2.0.0/gems/atomic-1.1.14/ext/siteconf20140112-11092-zez8vf.rb
+++ vendor/bundle/ruby/2.0.0/gems/atomic-1.1.14/ext/siteconf20140112-11092-zez8vf.rb
@@ -1,5 +1,5 @@
 require 'rbconfig'
-dest_path = "./.gem.20140112-10466-a0de8c"
+dest_path = "./.gem.20140112-11092-6r8s8"
 RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
 RbConfig::CONFIG['sitearchdir'] = dest_path
 RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
